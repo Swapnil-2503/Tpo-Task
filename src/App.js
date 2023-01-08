f038fd68e5589afca1ddccec75314c0d126a96dc
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-01-08 10:16:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HomeComponent from "./components/home.component";
+
+function App() {
+  return (
+    <main> 
+        <HomeComponent /> 
+    </main>
+  );
+}
+
+export default App;
+
